44e61c3305d487004f8fd0b41e62486791740941
Gabau <au.gabriel@yahoo.com.sg>
2021-12-28 17:07:12 +0800

    Add search feature for basic list

--- src/components/BasicList.tsx
+++ src/components/BasicList.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 const BasicList: React.FC = () => {
+    const defaultList = ['Frontend', 'Backend', 'Relational', 'MVC', 'RESTfulAPIs', 'Ruby on Rails', 'Go'];
+    const [list, setList] = useState(defaultList);
+    const [value, setValue] = useState('');
     return (
         <div style={{ width: '25vw', margin: 'auto', textAlign: 'center' }}>
+            <input
+                placeholder="search bar"
+                onChange={(event) => setValue(event.target.value)}
+                onKeyDown={(event) => {
+                    if (event.key != 'Enter') {
+                        return;
+                    }
+                    setList(defaultList.filter((val) => val.toLowerCase().includes(value.toLowerCase())));
+                }}
+            />
             <h4>{'Some web development tools/concepts:'}</h4>
             <ul>
-                <li>{'Frontend'}</li>
-                <li>{'Backend'}</li>
-                <li>{'Relational Database'}</li>
-                <li>{'MVC'}</li>
-                <li>{'RESTful APIs'}</li>
-                <li>{'Ruby on Rails'}</li>
-                <li>{'Go'}</li>
+                {list.map((val) => (
+                    <li key={val}>{val}</li>
+                ))}
             </ul>
         </div>
     );
